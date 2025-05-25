3bd972c4a8e2f981691bec20ba43b10e0bc42563
Eduardo Aguilar <earguila00@hotmail.com>
2025-05-25 10:34:08 +0100

    utils functions

---
+++ polynet/app/utils/__init__.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import pandas as pd
+
+
+def create_directory(path: Path):
+    """Create a directory at the specified path. If intermediate directories
+    don't already exist, create them. If the path already exists, no action
+    is taken.
+
+    Args:
+        path (Path): The path the directory to create.
+    """
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
+
+def keep_only_numerical_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Convert DataFrame columns to numeric where possible.
+    Drops any column that cannot be converted.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+
+    Returns:
+        pd.DataFrame: DataFrame containing only numerical columns.
+    """
+    numeric_df = pd.DataFrame()
+
+    for col in df.columns:
+        try:
+            numeric_col = pd.to_numeric(df[col], errors="raise")
+            numeric_df[col] = numeric_col
+        except (ValueError, TypeError):
+            # Column could not be converted to numeric; skip it
+            continue
+
+    return numeric_df
+
+
+def check_column_is_numeric(df, column_name: str) -> bool:
+    """Check if a column in a DataFrame is numeric.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to check.
+        column_name (str): The name of the column to check.
+
+    Returns:
+        bool: True if the column is numeric, False otherwise.
+    """
+    return pd.api.types.is_numeric_dtype(df[column_name]) if column_name in df.columns else False
