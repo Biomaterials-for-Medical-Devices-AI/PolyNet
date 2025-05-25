f0094fa2acadba8223204c3da3c4fb5072da11f5
Eduardo Aguilar <earguila00@hotmail.com>
2025-05-25 06:52:03 +0100

    add enums for node and edge features

--- polynet/options/enums.py
+++ polynet/options/enums.py
@@ -45,3 +45,32 @@ class Split_types(StrEnum):
     CrossValidation = "cross_validation"
     NestedCrossValidation = "nested_cross_validation"
     LeaveOneOut = "leave_one_out"
+
+
+class AtomBondDescriptorDictKeys(StrEnum):
+    AllowableVals = "allowable_vals"
+    Wildcard = "wildcard"
+    Options = "options"
+    Default = "default"
+    Description = "description"
+
+
+class AtomFeatures(StrEnum):
+    GetAtomicNum = "GetAtomicNum"
+    GetTotalDegree = "GetTotalDegree"
+    GetFormalCharge = "GetFormalCharge"
+    GetTotalNumHs = "GetTotalNumHs"
+    GetHybridization = "GetHybridization"
+    GetChiralTag = "GetChiralTag"
+    GetIsAromatic = "GetIsAromatic"
+    GetMass = "GetMass"
+    GetImplicitValence = "GetImplicitValence"
+    IsInRing = "IsInRing"
+
+
+class BondFeatures(StrEnum):
+    GetBondTypeAsDouble = "GetBondTypeAsDouble"
+    GetIsAromatic = "GetIsAromatic"
+    GetIsConjugated = "GetIsConjugated"
+    GetStereo = "GetStereo"
+    IsInRing = "IsInRing"
