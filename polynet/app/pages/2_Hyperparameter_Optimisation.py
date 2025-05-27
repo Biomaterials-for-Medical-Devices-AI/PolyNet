3f7284dca1ef47862c79ff1cd404a20c6bb10bfb
Eduardo Aguilar <earguila00@hotmail.com>
2025-05-27 14:36:41 +0100

    base structure for future pages

---
+++ polynet/app/pages/2_Hyperparameter_Optimisation.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from polynet.app.services.experiments import get_experiments
+from polynet.app.components.experiments import experiment_selector
+
+st.header("Representation of Polymers")
+
+st.markdown(
+    """
+    In this section, you can build the representation of your polymers. This representation will be the input for the ML models you will train. The representation is built using the SMILES strings of the monomers, which are the building blocks of the polymers.
+    We currently support two types of representations:
+    1. **Graph Representation**: This representation is built using the SMILES strings of the monomers. The graph representation is built using the RDKit library, which is a collection of cheminformatics and machine learning tools. For this representation, the SMILES strings are converted into a graph representation, where the atoms are the nodes and the bonds are the edges. This representation is used to build the graph neural networks (GNNs) that will be trained on your dataset.
+    2. **Molecular Descriptors**: This representation is built using the RDKit library. The molecular descriptors are a set of numerical values that describe the structure and properties of the molecule. This approach effectively transforms the molecule into a vector representation. You can also use descriptors from the dataset, which you can concatenate with the RDkit descriptors or use them as a separate input to the model.
+    """
+)
+
+
+choices = get_experiments()
+experiment_name = experiment_selector(choices)
