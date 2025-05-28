a9b4794ec03e27caacc29d8e3d5962951451a27e
Eduardo Aguilar <earguila00@hotmail.com>
2025-05-28 12:19:18 +0100

    add train set balance option

--- polynet/app/options/general_experiment.py
+++ polynet/app/options/general_experiment.py
@@ -6,6 +6,7 @@ class GeneralConfigOptions:
     """Data split options for the application."""
 
     split_method: str
+    train_set_balance: float
     test_ratio: str
     val_ratio: str
     random_seed: int
