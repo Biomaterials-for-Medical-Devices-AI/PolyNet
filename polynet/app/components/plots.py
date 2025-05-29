79769ade1666c169675bc13fa1011727e1ad9eaf
Eduardo Aguilar <earguila00@hotmail.com>
2025-05-29 13:54:23 +0100

    functions to display plots and predictions

---
+++ polynet/app/components/plots.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+
+def display_predictions(predictions_df: pd.DataFrame):
+    """
+    Display the predictions in the UI.
+
+    Args:
+        predictions_path (Path): The path to the predictions file.
+    """
+    st.write("### Predictions")
+    st.write(predictions_df)
+
+
+def display_plots(plots_path):
+
+    for plot_file in plots_path.glob("*.png"):
+        # Display each plot file in the directory
+        st.image(plot_file, use_container_width=True)
