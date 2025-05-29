de867190fb487a9a0d50460d173ecea42e91ff3d
Eduardo Aguilar <earguila00@hotmail.com>
2025-05-29 13:55:26 +0100

    save results and display

--- polynet/app/pages/3_Train_Models.py
+++ polynet/app/pages/3_Train_Models.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 from polynet.app.components.experiments import experiment_selector
 from polynet.app.components.forms.train_models import (
@@ -12,11 +13,15 @@ from polynet.app.options.file_paths import (
     general_options_path,
     gnn_raw_data_file,
     gnn_raw_data_path,
+    gnn_model_dir,
     polynet_experiments_base_dir,
     representation_file,
     representation_file_path,
     representation_options_path,
     train_gnn_model_options_path,
+    gnn_plots_directory,
+    ml_gnn_results_file_path,
+    ml_results_parent_directory,
 )
 from polynet.app.options.general_experiment import GeneralConfigOptions
 from polynet.app.options.representation import RepresentationOptions
@@ -29,9 +34,13 @@ from polynet.app.options.train_GNN import TrainGNNOptions
 from polynet.app.services.configurations import load_options, save_options
 from polynet.app.services.experiments import get_experiments
 from polynet.app.services.train_gnn import predict_gnn_model, train_network
+from polynet.app.services.model_training import save_gnn_model, save_plot
+from polynet.app.components.plots import display_plots, display_predictions
 from polynet.options.enums import DataSets, ProblemTypes, Results
 from polynet.utils.model_training import predict_network
 from polynet.utils.plot_utils import plot_confusion_matrix
+from polynet.app.utils import save_data, merge_model_predictions
+from shutil import rmtree
 
 
 def train_models(
@@ -66,15 +75,19 @@ def train_models(
 
     experiment_path = polynet_experiments_base_dir() / experiment_name
 
-    save_options(
-        path=train_gnn_model_options_path(experiment_path=experiment_path),
-        options=train_gnn_options,
-    )
+    gnn_training_opts_path = train_gnn_model_options_path(experiment_path=experiment_path)
+    gen_options_path = general_options_path(experiment_path=experiment_path)
+    ml_results_dir = ml_results_parent_directory(experiment_path=experiment_path)
 
-    save_options(
-        path=general_options_path(experiment_path=experiment_path),
-        options=general_experiment_options,
-    )
+    if gnn_training_opts_path.exists():
+        gnn_training_opts_path.unlink()
+    if gen_options_path.exists():
+        gen_options_path.unlink()
+    if ml_results_dir.exists():
+        rmtree(ml_results_dir)
+
+    save_options(path=gnn_training_opts_path, options=train_gnn_options)
+    save_options(path=gen_options_path, options=general_experiment_options)
 
     model, loaders = train_network(
         train_gnn_options=train_gnn_options,
@@ -84,19 +97,47 @@ def train_models(
         representation_options=representation_options,
     )
 
+    predictions_all = []
+
+    gnn_models_dir = gnn_model_dir(experiment_path=experiment_path)
+    gnn_models_dir.mkdir(parents=True, exist_ok=True)
+
+    gnn_plots_dir = gnn_plots_directory(experiment_path=experiment_path)
+    gnn_plots_dir.mkdir(parents=True, exist_ok=True)
+
     for model_name, model in model.items():
 
+        save_path = gnn_models_dir / f"{model_name}.pt"
+        save_gnn_model(model=model, path=save_path)
+
         predictions = predict_gnn_model(model=model, loaders=loaders)
+        predictions_all.append(predictions)
 
         if data_options.problem_type == ProblemTypes.Classification:
-            predictions_test = predictions.loc[predictions["Set"] == DataSets.Test]
+            predictions_test = predictions.loc[predictions[Results.Set.value] == DataSets.Test]
             fig = plot_confusion_matrix(
-                y_true=predictions_test[Results.Label.value],
-                y_pred=predictions_test[Results.Predicted.value],
-                title=f"Confusion Matrix for {model_name}",
+                y_true=predictions_test.iloc[:, 2],
+                y_pred=predictions_test.iloc[:, 3],
+                display_labels=(
+                    list(data_options.class_names.values()) if data_options.class_names else None
+                ),
+                title=f"{data_options.target_variable_name}\nConfusion Matrix for\n {model_name}",
             )
+            save_plot_path = gnn_plots_dir / f"{model_name}_confusion_matrix.png"
+            save_plot(fig=fig, path=save_plot_path)
+        else:
+            pass
 
-            st.pyplot(fig, clear_figure=True)
+    predictions_all = merge_model_predictions(dfs=predictions_all)
+
+    save_data(
+        data=predictions_all,
+        data_path=ml_gnn_results_file_path(
+            experiment_path=experiment_path, file_name="predictions.csv"
+        ),
+    )
+    display_predictions(predictions_df=predictions_all)
+    display_plots(plots_path=gnn_plots_dir)
 
 
 st.header("Train Models")
@@ -123,14 +164,39 @@ if experiment_name:
 
     data_opts = load_options(path=path_to_data_opts, options_class=DataOptions)
 
-    # st.write(data_opts)
     path_to_representation_opts = representation_options_path(
         experiment_path=polynet_experiments_base_dir() / experiment_name
     )
     representation_opts = load_options(
         path=path_to_representation_opts, options_class=RepresentationOptions
     )
-    # st.write(representation_opts)
+
+    train_gnn_options = train_gnn_model_options_path(
+        experiment_path=polynet_experiments_base_dir() / experiment_name
+    )
+
+    if train_gnn_options.exists():
+        st.error(
+            "GNN model options already exist for this experiment. "
+            "You can modify the settings below, but be aware that this will overwrite the existing results."
+        )
+
+        with st.expander("View Previous GNN Model Results", expanded=True):
+
+            st.write("### Previous GNN Training Results")
+
+            train_gnn_options = load_options(
+                path=train_gnn_model_options_path(experiment_path=experiment_path),
+                options_class=TrainGNNOptions,
+            )
+            predictions = pd.read_csv(
+                ml_gnn_results_file_path(
+                    experiment_path=experiment_path, file_name="predictions.csv"
+                )
+            )
+            plots_path = gnn_plots_directory(experiment_path=experiment_path)
+            display_predictions(predictions_df=predictions)
+            display_plots(plots_path=plots_path)
 
     st.markdown("## Train Machine Learning Models (TMLs)")
 
