d9ffa828e13ed5d4fd356a9453b6aed2e2dcc187
Eduardo Aguilar <earguila00@hotmail.com>
2025-05-31 16:16:23 +0100

    logic to handle any model in GUI

--- polynet/app/services/train_gnn.py
+++ polynet/app/services/train_gnn.py
@@ -15,9 +15,17 @@ from polynet.app.options.train_GNN import TrainGNNOptions
 from polynet.app.services.model_training import split_data
 from polynet.call_methods import create_network, make_loss, make_optimizer, make_scheduler
 from polynet.featurizer.graph_representation.polymer import CustomPolymerGraph
-from polynet.options.enums import DataSets, Optimizers, Results, Schedulers, SplitMethods
+from polynet.options.enums import (
+    DataSets,
+    Optimizers,
+    Results,
+    Schedulers,
+    SplitMethods,
+    SplitTypes,
+)
 from polynet.utils.data_preprocessing import class_balancer
 from polynet.utils.model_training import predict_network, train_model
+from polynet.app.utils import get_true_label_column_name, get_predicted_label_column_name
 
 
 def train_network(
@@ -35,10 +43,6 @@ def train_network(
         index_col=0,
     )
 
-    train_ids, val_ids, test_ids = get_data_split_indices(
-        data=data, data_options=data_options, general_experiment_options=general_experiment_options
-    )
-
     # === Step 2: Create dataset and filter splits
     dataset = CustomPolymerGraph(
         filename=data_options.data_name,
@@ -51,59 +55,76 @@ def train_network(
         edge_feats=representation_options.edge_feats,
     )
 
+    train_ids, val_ids, test_ids = get_data_split_indices(
+        data=data, data_options=data_options, general_experiment_options=general_experiment_options
+    )
+
     def filter_dataset_by_ids(dataset, ids):
         return [data for data in dataset if data.idx in ids]
-
-    train_set = filter_dataset_by_ids(dataset, train_ids)
-    val_set = filter_dataset_by_ids(dataset, val_ids)
-    test_set = filter_dataset_by_ids(dataset, test_ids)
-
-    # === Step 3: Prepare dataloaders
-    batch_size = train_gnn_options.GNNBatchSize
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
-    loaders = (train_loader, val_loader, test_loader)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     trained_models = {}
 
     # === Step 4: Train one model for each GNN architecture
-    for gnn_arch, arch_params in train_gnn_options.GNNConvolutionalLayers.items():
-        st_msg = f"Training model with architecture: {gnn_arch}"
-        print(st_msg)
+    for iteration, (train_idxs, val_idxs, test_idxs) in enumerate(
+        zip(train_ids, val_ids, test_ids)
+    ):
 
-        model_kwargs = {
-            "n_node_features": dataset[0].num_node_features,
-            "n_edge_features": dataset[0].num_edge_features,
-            "pooling": train_gnn_options.GNNPoolingMethod,
-            "n_convolutions": train_gnn_options.GNNNumberOfLayers,
-            "embedding_dim": train_gnn_options.GNNEmbeddingDimension,
-            "readout_layers": train_gnn_options.GNNReadoutLayers,
-            "n_classes": int(data_options.num_classes),
-            "dropout": train_gnn_options.GNNDropoutRate,
-            "seed": general_experiment_options.random_seed,
-        }
+        trained_models[iteration] = {}
+        trained_models[iteration][Results.Model.value] = {}
 
-        all_kwargs = {**model_kwargs, **arch_params}
+        train_set = filter_dataset_by_ids(dataset, train_idxs)
+        val_set = filter_dataset_by_ids(dataset, val_idxs)
+        test_set = filter_dataset_by_ids(dataset, test_idxs)
 
-        model = create_network(
-            network=gnn_arch, problem_type=data_options.problem_type, **all_kwargs
-        )
+        # === Step 3: Prepare dataloaders
+        batch_size = train_gnn_options.GNNBatchSize
+        train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+        val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
+        test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
-        optimizer = make_optimizer(Optimizers.Adam, model, lr=train_gnn_options.GNNLearningRate)
-        scheduler = make_scheduler(
-            Schedulers.ReduceLROnPlateau, optimizer, step_size=15, gamma=0.9, min_lr=1e-8
-        )
-        loss_fn = make_loss(model.problem_type)
+        trained_models[iteration][Results.Loaders.value] = (train_loader, val_loader, test_loader)
 
-        model = train_model(
-            model, train_loader, val_loader, test_loader, loss_fn, optimizer, scheduler, device
-        )
+        for gnn_arch, arch_params in train_gnn_options.GNNConvolutionalLayers.items():
+            st_msg = f"Training model with architecture: {gnn_arch}"
 
-        trained_models[gnn_arch] = model
+            model_kwargs = {
+                "n_node_features": dataset[0].num_node_features,
+                "n_edge_features": dataset[0].num_edge_features,
+                "pooling": train_gnn_options.GNNPoolingMethod,
+                "n_convolutions": train_gnn_options.GNNNumberOfLayers,
+                "embedding_dim": train_gnn_options.GNNEmbeddingDimension,
+                "readout_layers": train_gnn_options.GNNReadoutLayers,
+                "n_classes": int(data_options.num_classes),
+                "dropout": train_gnn_options.GNNDropoutRate,
+                "seed": general_experiment_options.random_seed + iteration,
+            }
 
-    return trained_models, loaders
+            all_kwargs = {**model_kwargs, **arch_params}
+
+            model = create_network(
+                network=gnn_arch, problem_type=data_options.problem_type, **all_kwargs
+            )
+
+            optimizer = make_optimizer(Optimizers.Adam, model, lr=train_gnn_options.GNNLearningRate)
+            scheduler = make_scheduler(
+                Schedulers.ReduceLROnPlateau, optimizer, step_size=15, gamma=0.9, min_lr=1e-8
+            )
+            loss_fn = make_loss(model.problem_type)
+
+            model = train_model(
+                model=model,
+                train_loader=train_loader,
+                val_loader=val_loader,
+                test_loader=test_loader,
+                loss=loss_fn,
+                optimizer=optimizer,
+                scheduler=scheduler,
+                device=device,
+            )
+            trained_models[iteration][Results.Model.value][gnn_arch] = model
+
+    return trained_models
 
 
 def predict_gnn_model(model, loaders, target_variable_name=None):
@@ -142,7 +163,9 @@ def predict_gnn_model(model, loaders, target_variable_name=None):
         model_name=model._name,
     )
 
-    predictions = pd.concat([predictions_train, predictions_val, prediction_test])
+    predictions = pd.concat(
+        [predictions_train, predictions_val, prediction_test], ignore_index=True
+    )
 
     return predictions
 
@@ -157,16 +180,11 @@ def create_results_dataframe(
     model_name: str = None,
 ):
 
-    true_label = (
-        f"{Results.Label.value} {target_variable_name}"
-        if target_variable_name
-        else Results.Label.value
+    true_label = get_true_label_column_name(target_variable_name=target_variable_name)
+    predicted_label = get_predicted_label_column_name(
+        target_variable_name=target_variable_name, model_name=model_name
     )
-    predicted_label = (
-        f"{model_name} {Results.Predicted.value} {target_variable_name}"
-        if target_variable_name
-        else f"{model_name} {Results.Predicted.value}"
-    )
+
     return pd.DataFrame(
         {
             Results.Index.value: idx,
@@ -180,36 +198,47 @@ def create_results_dataframe(
 def get_data_split_indices(
     data: pd.DataFrame, data_options: DataOptions, general_experiment_options: GeneralConfigOptions
 ):
-    # Initial train-test split
-    train_data, test_data = split_data(
-        data=data,
-        test_size=general_experiment_options.test_ratio,
-        stratify=(
-            data[data_options.target_variable_col]
-            if general_experiment_options.split_method == SplitMethods.Stratified
-            else None
-        ),
-        random_state=general_experiment_options.random_seed,
-    )
 
-    # Optional class balancing on training set
-    if general_experiment_options.train_set_balance:
-        train_data = class_balancer(
-            data=train_data,
-            target=data_options.target_variable_col,
-            desired_class_proportion=general_experiment_options.train_set_balance,
-        )
+    if general_experiment_options.split_type == SplitTypes.TrainValTest:
 
-    # Further split train into train/validation
-    train_data, val_data = split_data(
-        data=train_data,
-        test_size=general_experiment_options.val_ratio,
-        stratify=(
-            train_data[data_options.target_variable_col]
-            if general_experiment_options.split_method == SplitMethods.Stratified
-            else None
-        ),
-        random_state=general_experiment_options.random_seed,
-    )
+        train_data_idxs, val_data_idxs, test_data_idxs = [], [], []
 
-    return train_data.index, val_data.index, test_data.index
+        for i in range(general_experiment_options.n_bootstrap_iterations):
+            # Initial train-test split
+            train_data, test_data = split_data(
+                data=data,
+                test_size=general_experiment_options.test_ratio,
+                stratify=(
+                    data[data_options.target_variable_col]
+                    if general_experiment_options.split_method == SplitMethods.Stratified
+                    else None
+                ),
+                random_state=general_experiment_options.random_seed + i,
+            )
+
+            # Optional class balancing on training set
+            if general_experiment_options.train_set_balance:
+                train_data = class_balancer(
+                    data=train_data,
+                    target=data_options.target_variable_col,
+                    desired_class_proportion=general_experiment_options.train_set_balance,
+                    random_state=general_experiment_options.random_seed + i,
+                )
+
+            # Further split train into train/validation
+            train_data, val_data = split_data(
+                data=train_data,
+                test_size=general_experiment_options.val_ratio,
+                stratify=(
+                    train_data[data_options.target_variable_col]
+                    if general_experiment_options.split_method == SplitMethods.Stratified
+                    else None
+                ),
+                random_state=general_experiment_options.random_seed + i,
+            )
+
+            train_data_idxs.append(train_data.index.tolist())
+            val_data_idxs.append(val_data.index.tolist())
+            test_data_idxs.append(test_data.index.tolist())
+
+    return train_data_idxs, val_data_idxs, test_data_idxs
