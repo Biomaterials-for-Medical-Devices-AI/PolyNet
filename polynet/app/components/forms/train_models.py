3f2ef911416686688464a9d4abf7751982b27bf1
Eduardo Aguilar <earguila00@hotmail.com>
2025-07-15 14:50:07 +0100

    add floats to min and max value

--- polynet/app/components/forms/train_models.py
+++ polynet/app/components/forms/train_models.py
@@ -177,11 +177,10 @@ def train_GNN_models_form(representation_opts: RepresentationOptions, problem_ty
         ):
             st.slider(
                 "Set the imbalance strength",
-                min_value=0,
-                max_value=1,
+                min_value=0.0,
+                max_value=1.0,
                 value=0.5,
                 step=0.1,
-                format=float,
                 key=TrainGNNStateKeys.ImbalanceStrength,
                 help="Controls how much strength to apply to the asymmetric loss function. A value of 1 means that weighting will be given by the inverse of the total count of each label, while a value of 0 means that each class will be weighted equally.",
             )
