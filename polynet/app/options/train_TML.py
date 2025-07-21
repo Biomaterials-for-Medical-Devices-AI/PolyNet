255c8950827e08ec54372687698fed9b4b36fd98
Eduardo Aguilar <earguila00@hotmail.com>
2025-07-21 10:44:38 +0100

    add option to transform features

--- polynet/app/options/train_TML.py
+++ polynet/app/options/train_TML.py
@@ -7,8 +7,9 @@ class TrainTMLOptions:
     Options for training traditional machine learning models.
     """
 
+    TransformFeatures: str = "NoTransformation"
     TrainLinearRegression: bool = True
     TrainLogisticRegression: bool = True
     TrainRandomForest: bool = True
-    TrainXGBoost: bool = True
     TrainSupportVectorMachine: bool = True
+    TrainXGBoost: bool = True
