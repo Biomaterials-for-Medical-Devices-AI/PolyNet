10f98ddff20082531e479bb24ddf91ed3da493a4
Eduardo Aguilar <earguila00@hotmail.com>
2025-07-28 15:08:45 +0100

    add utils to deal with psmiles

--- polynet/utils/chem_utils.py
+++ polynet/utils/chem_utils.py
@@ -4,8 +4,28 @@ from typing import Optional
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import BRICS
+from psmiles import PolymerSmiles
 
 from polynet.options.enums import AtomFeatures, BondFeatures
+from polynet.options.enums import StringRepresentation
+from canonicalize_psmiles.canonicalize import canonicalize as ext_canonicalize
+
+
+class PS(PolymerSmiles):
+    def __init__(self, psmiles, deactivate_warnings=True):
+        super().__init__(psmiles, deactivate_warnings)
+        self.deactivate_warnings = deactivate_warnings
+
+    @property
+    def canonicalize(self) -> PolymerSmiles:
+        """Canonicalize the PSMILES string
+
+        Returns:
+            PolymerSmiles: canonicalized PSMILES string
+        """
+        return PolymerSmiles(
+            ext_canonicalize(self.psmiles), deactivate_warnings=self.deactivate_warnings
+        )
 
 
 def check_smiles(smiles: str) -> bool:
@@ -43,6 +63,26 @@ def canonicalise_smiles(smiles: str) -> Optional[str]:
         return Chem.MolToSmiles(mol, canonical=True)
     except Exception as e:
         print(f"Error converting SMILES to canonical form: '{smiles}'. Exception: {e}")
+        return None
+
+
+def identify_psmiles(smiles: str):
+    return smiles.count("*") >= 2
+
+
+def determine_string_representation(df, smiles_cols):
+    for col in smiles_cols:
+        if not df[col].apply(identify_psmiles).all():
+            return StringRepresentation.Smiles
+    return StringRepresentation.PSmiles
+
+
+def canonicalise_psmiles(psmiles: str) -> Optional[str]:
+    try:
+        psmiles = PS(psmiles=psmiles, deactivate_warnings=True).canonicalize
+        return psmiles.psmiles
+    except Exception as e:
+        print(f"Error converting SMILES to canonical form: '{psmiles}'. Exception: {e}")
         return None
 
 
