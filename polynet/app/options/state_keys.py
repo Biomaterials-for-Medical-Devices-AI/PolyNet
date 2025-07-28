13b7aa097ebcaa803bb9098b91daef65413998f3
Eduardo Aguilar <earguila00@hotmail.com>
2025-07-28 15:27:45 +0100

    add state key for calculation of polybert

--- polynet/app/options/state_keys.py
+++ polynet/app/options/state_keys.py
@@ -36,6 +36,7 @@ class DescriptorCalculationStateKeys(StrEnum):
     MergeDescriptors = "MergeDescriptors"
     GraphWeightingFactor = "GraphWeightingFactor"
     DescriptorsRDKit = "DescriptorsRDKit"
+    polyBERTfp = "polyBERTfp"
     SelectAllRDKitDescriptors = "SelectAllRDKitDescriptors"
     IndependentDFDescriptors = "IndependentDFDescriptors"
     IndependentRDKitDescriptors = "IndependentRDKitDescriptors"
