f4297da7544ee8007ec62667ad052f0df8fa2778
Eduardo Aguilar <earguila00@hotmail.com>
2025-08-14 10:22:07 +0100

    sort experiments

--- polynet/app/services/experiments.py
+++ polynet/app/services/experiments.py
@@ -32,7 +32,7 @@ def get_experiments(base_dir: Path | None = None) -> list[str]:
     experiments = filter(lambda x: not x.startswith("."), experiments)
     # Filter out files
     experiments = filter(lambda x: os.path.isdir(os.path.join(base_dir, x)), experiments)
-    return list(experiments)
+    return sorted(list(experiments))
 
 
 def create_experiment(save_dir: Path, data_options: DataOptions):
