ee43aafbf1e353d0a6f4aa51c238f3f9101a11dd
Eduardo Aguilar <earguila00@hotmail.com>
2025-10-31 13:54:46 +0000

    take out old functions

--- polynet/app/services/model_training.py
+++ polynet/app/services/model_training.py
@@ -1,29 +1,11 @@
 from pathlib import Path
-from math import sqrt
 
-from imblearn.metrics import geometric_mean_score as gmean
-from imblearn.metrics import specificity_score
 import joblib
-import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score, f1_score
-from sklearn.metrics import (
-    mean_absolute_error,
-    mean_squared_error,
-    precision_score,
-    r2_score,
-    recall_score,
-    roc_auc_score,
-)
-from sklearn.metrics import matthews_corrcoef as mcc
-import torch
 from torch import load, save
-from torch_geometric.data import Dataset
-from torch_geometric.loader import DataLoader
 
 from polynet.app.options.file_paths import gnn_model_dir, representation_file
 from polynet.app.options.representation import RepresentationOptions
-from polynet.options.enums import EvaluationMetrics, ProblemTypes
 from polynet.featurizer.preprocess import sanitise_df
 
 
@@ -75,72 +57,6 @@ def save_plot(fig, path, dpi=300):
     """
     fig.savefig(path, bbox_inches="tight", dpi=dpi)
     print(f"Plot saved to {path}")
-
-
-def calculate_standard_error(actual_values, predicted_values):
-    return np.sqrt(np.mean((actual_values - predicted_values) ** 2))
-
-
-def calculate_sep(y_true, y_pred):
-    """
-    Calculates the Standard Error of Prediction (SEP).
-
-    Parameters:
-    - y_true: array-like of shape (n_samples,), ground truth values.
-    - y_pred: array-like of shape (n_samples,), predicted values.
-
-    Returns:
-    - sep: float, the standard error of prediction.
-    """
-    y_true = np.asarray(y_true)
-    y_pred = np.asarray(y_pred)
-
-    if len(y_true) != len(y_pred):
-        raise ValueError("y_true and y_pred must be the same length")
-
-    residuals = y_true - y_pred
-    sep = np.sqrt(np.sum(residuals**2) / (len(residuals) - 1))
-    return sep
-
-
-def calculate_metrics(y_true, y_pred, y_probs, problem_type):
-    """
-    Calculates evaluation metrics based on the problem type.
-
-    Args:
-        y_true (pd.Series): True labels.
-        y_pred (pd.Series): Predicted labels.
-        problem_type (str): Type of the problem ('classification' or 'regression').
-
-    Returns:
-        dict: Dictionary containing calculated metrics.
-    """
-    if problem_type == ProblemTypes.Classification:
-
-        return {
-            EvaluationMetrics.Accuracy: accuracy_score(y_true, y_pred),
-            EvaluationMetrics.Precision: precision_score(y_true, y_pred),
-            EvaluationMetrics.Recall: recall_score(
-                y_true, y_pred
-            ),  # Also known as sensitivity or true positive rate
-            EvaluationMetrics.Specificity: specificity_score(
-                y_true, y_pred
-            ),  # Recall for negative samples or True negative rate
-            EvaluationMetrics.AUROC: (
-                roc_auc_score(y_true=y_true, y_score=y_probs) if y_probs is not None else None
-            ),
-            EvaluationMetrics.MCC: mcc(y_true, y_pred),
-            EvaluationMetrics.F1Score: f1_score(y_true, y_pred),
-            EvaluationMetrics.GScore: gmean(y_true, y_pred),
-        }
-    else:
-
-        return {
-            EvaluationMetrics.RMSE: sqrt(mean_squared_error(y_true, y_pred)),
-            EvaluationMetrics.R2: r2_score(y_true, y_pred),
-            EvaluationMetrics.MAE: mean_absolute_error(y_true, y_pred),
-            # "sep": calculate_standard_error(y_true, y_pred),
-        }
 
 
 def load_dataframes(
@@ -260,36 +176,3 @@ def load_scalers_from_experiment(experiment_path: str, model_names: list) -> dic
         scaler[scaler_name] = load_tml_model(scaler_file)
 
     return scaler
-
-
-def predict_network(models: dict, dataset: Dataset) -> pd.DataFrame:
-
-    indexes = [data.idx for data in dataset]
-    loader = DataLoader(dataset=dataset, batch_size=1, shuffle=False)
-
-    predictions_models = {}
-
-    for model_name, model in models.items():
-        model.eval()
-        predictions = []
-
-        with torch.no_grad():
-            for batch in loader:
-
-                preds = model.predict(
-                    x=batch.x,
-                    edge_index=batch.edge_index,
-                    edge_attr=batch.edge_attr,
-                    batch_index=batch.batch,
-                    monomer_weight=batch.weight_monomer,
-                )
-
-                predictions.append(preds)
-
-        predictions_models[model_name] = pd.Series(
-            np.concatenate(predictions), index=indexes, name=model_name
-        )
-
-    predictions_models = pd.DataFrame(predictions_models)
-
-    return predictions_models
