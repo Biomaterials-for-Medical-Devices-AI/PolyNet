b13b5bae84f1f6fe97f1569ada0244ca6f2371fc
Eduardo Aguilar <earguila00@hotmail.com>
2025-10-31 14:39:06 +0000

    move functions from app to correct directories

--- polynet/featurizer/preprocess.py
+++ polynet/featurizer/preprocess.py
@@ -34,3 +34,12 @@ def sanitise_df(df: pd.DataFrame, descriptors: list, target_variable_col: str):
     clean_df = clean_df[descriptors + [target_variable_col]].dropna(axis=1)
 
     return clean_df
+
+
+def get_data_index(
+    data: pd.DataFrame, id_col: str, smiles_cols: list, weights_col: dict, target_col: str
+):
+    idx_cols = [id_col] + smiles_cols + list(weights_col.values())
+    if target_col in data.columns:
+        idx_cols += [target_col]
+    return data[idx_cols].copy()
