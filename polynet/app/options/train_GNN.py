22427a84a2a4a55a43a82416cfd9627f9deb14db
Eduardo Aguilar <earguila00@hotmail.com>
2025-11-14 15:50:53 +0000

    Add trainGNN option

--- polynet/app/options/train_GNN.py
+++ polynet/app/options/train_GNN.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class TrainGNNOptions:
-
     GNNConvolutionalLayers: dict[str, dict]
+    TrainGNN: bool = True
     HyperparameterOptimisation: bool = False
     ShareGNNParameters: bool = True
     TrainGNNModel: bool = True
