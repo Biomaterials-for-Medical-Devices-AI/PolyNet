98b2147b8e2fb9698cac9b8e13086ca03591efc7
Eduardo Aguilar <earguila00@hotmail.com>
2026-03-18 15:07:49 +0000

    add feature transform config to API

--- polynet/config/schemas/__init__.py
+++ polynet/config/schemas/__init__.py
@@ -17,6 +17,7 @@ Import directly from this package for convenience::
 
 from polynet.config.schemas.data import DataConfig
 from polynet.config.schemas.general import GeneralConfig
+from polynet.config.schemas.feature_preprocessing import FeatureTransformConfig
 from polynet.config.schemas.plotting import PlottingConfig
 from polynet.config.schemas.representation import RepresentationConfig
 from polynet.config.schemas.training import TrainGNNConfig, TrainTMLConfig
@@ -24,6 +25,7 @@ from polynet.config.schemas.training import TrainGNNConfig, TrainTMLConfig
 __all__ = [
     "DataConfig",
     "GeneralConfig",
+    "FeatureTransformConfig",
     "PlottingConfig",
     "RepresentationConfig",
     "TrainGNNConfig",
