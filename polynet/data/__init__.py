8d2b32ef1ef4d7e72a19aab008da5d1fce6208c1
Eduardo Aguilar <earguila00@hotmail.com>
2026-04-11 10:23:17 +0100

    init target scaler

--- polynet/data/__init__.py
+++ polynet/data/__init__.py
@@ -5,23 +5,23 @@ Data loading and preprocessing utilities for the polynet pipeline.
 
 ::
 
-    from polynet.data import load_dataset, class_balancer, transform_features
+    from polynet.data import load_dataset, class_balancer, TargetScaler
 """
 
 from polynet.data.creator import DatasetCreator
 from polynet.data.feature_transformer import FeatureTransformer
 from polynet.data.loader import load_dataset
 from polynet.data.preprocessing import (
+    TargetScaler,
     class_balancer,
     get_data_index,
     sanitise_df,
-    transform_features,
 )
 
 __all__ = [
     "load_dataset",
     "class_balancer",
-    "transform_features",
+    "TargetScaler",
     "sanitise_df",
     "get_data_index",
     "FeatureTransformer",
